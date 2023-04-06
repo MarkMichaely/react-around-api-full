bea5ed980241be9d05d31e4348ef83870e8984a8
mark <markmichaely@gmail.com>
2023-04-06 11:53:14 +0300

    added error classes and changed error handling for app and cards

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,22 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
-const { NOTFOUND } = require('./utils/errors');
+const { SERVERERROR } = require('./utils/errors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middleware/auth');
+const { errors } = require('celebrate');
+const NotFoundError = require('./errors/not-found-error');
 require('dotenv').config();
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 app.use(helmet());
 const jsonParser = bodyParser.json();
 
 mongoose.connect('mongodb://localhost:27017/aroundb');
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 app.use(auth);
@@ -25,9 +28,19 @@ app.use('/', jsonParser, usersRouter);
 app.use('/', jsonParser, cardsRouter);
 
 app.use('*', (req, res) => {
-  res.status(NOTFOUND).json({ message: 'Requested resource not found' });
+  throw new NotFoundError('Requested resource not found');
 });
-
+app.use(errors());
+app.use((err, req, res, next) => {
+  const { statusCode = SERVERERROR, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === SERVERERROR
+        ? 'An error occurred on the server'
+        : message
+    });
+});
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
 });
--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,72 +1,72 @@
+const BadRequestError = require('../errors/bad-request-error');
+const ForbiddenError = require('../errors/forbidden-err');
+const NotFoundError = require('../errors/not-found-error');
+const ServerError = require('../errors/server-err');
 const Card = require('../models/card');
 const { BADREQUEST, NOTFOUND, SERVERERROR, FORBIDDEN } = require('../utils/errors');
 
-const getCards = (req, res) => {
+const getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch(() => res.status(SERVERERROR).send({ message: 'An error has occured on the server' }));
+    .catch(() => {
+      next(new ServerError('An error has occured on the server'));
+    });
 };
 
-const createCard = (req, res) => {
+const createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user })
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === 'ValidationError') res.status(BADREQUEST).send({ message: 'Wrong data for card' });
-      else res.status(SERVERERROR).send({ message: 'An error has occured on the server' });
+      if (err.name === 'ValidationError') next(new BadRequestError('Wrong data for card'));
+      else next(err);
     });
 };
 
-const deleteCard = (req, res) => {
+const deleteCard = (req, res, next) => {
   if (req.params.id !== req.user._id) {
-    res.status(FORBIDDEN).send({ message: 'Access Forbidden' });
+    throw new ForbiddenError('Access Forbidden');
   }
   Card.findByIdAndRemove(req.params.id)
     .then((doc) => {
       if (doc) {
         res.send({ message: 'Card succesfully removed' });
-      } else res.status(NOTFOUND).send({ message: 'No card found' });
+      } else throw NotFoundError('No card found');
     }).catch((err) => {
-      if (err.name === 'CastError') res.status(BADREQUEST).send({ message: 'unsupported cardId' });
-      res.status(SERVERERROR).send({ message: 'An error has occured on the server' });
+      if (err.name === 'CastError') next(new BadRequestError('unsupported cardId'));
+      else next(err);
     });
 };
 
-const likeCard = (req, res) => {
+const likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
     .orFail(() => {
-      const error = new Error('card not found');
-      error.statusCode = NOTFOUND;
-      throw error;
+      throw new NotFoundError('card not found');
     })
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === 'CastError') res.status(BADREQUEST).send({ message: 'unsupported cardId' });
-      else if (err.status === NOTFOUND) res.status(NOTFOUND).send({ message: err.message });
-      else res.status(SERVERERROR).send({ message: 'An error has occured on the server' });
+      if (err.name === 'CastError') next(new BadRequestError('unsupported cardId'));
+      else next(err);
     });
 };
 
-const unLikeCard = (req, res) => {
+const unLikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
     .orFail(() => {
-      const error = new Error('card not found');
-      error.statusCode = NOTFOUND;
-      throw error;
+      throw new NotFoundError('card not found');
     })
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === 'CastError') res.status(BADREQUEST).send({ message: 'unsupported cardId' });
-      else if (err.status === NOTFOUND) res.status(NOTFOUND).send({ message: err.message });
-      else res.status(SERVERERROR).send({ message: 'An error has occured on the server' });
+      if (err.name === 'CastError') next(new BadRequestError('unsupported cardId'));
+      else next(err);
     });
 };
 module.exports = {
